cb23d6ce4c033fa067c717a03c798186a958adf2
Alexey Rumyntsev <alrumbl4@yandex.ru>
2024-02-23 07:35:00 +0300

    Готовое задание

---
+++ test_items.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_button(browser):
+    browser.get(link)
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    time.sleep(5)
+    assert "Añadir al carrito" in button.text
